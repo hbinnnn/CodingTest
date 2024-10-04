1cca9462f43c5f1308f7d44d92999ffbe34a4f42
hbinnnn <106761437+hbinnnn@users.noreply.github.com>
2024-10-04 21:19:24 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.06 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        return myString.replace("A", "X").replace("B","A").replace("X","B").contains(pat) ? 1 : 0;
+    }
+}
