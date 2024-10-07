6754cb9006787bd76f9251f04d1a733f78df16b4
hbinnnn <106761437+hbinnnn@users.noreply.github.com>
2024-10-07 19:53:03 +0900

    [level 0] Title: 접미사 배열, Time: 0.80 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,21 +1,14 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = {};
-        ArrayList<String> strList = new ArrayList<>();
+        String[] answer = new String[my_string.length()];
         
         for(int i=0;i<my_string.length();i++){
-            strList.add(my_string.substring(i, my_string.length()));
+            answer[i] = my_string.substring(i, my_string.length());
         }
-        Collections.sort(strList);
+        Arrays.sort(answer);
         
-        answer = new String[strList.size()];
-        
-        for(int j=0;j<strList.size();j++){
-            answer[j] = strList.get(j);
-        }
         return answer;
     }
 }
