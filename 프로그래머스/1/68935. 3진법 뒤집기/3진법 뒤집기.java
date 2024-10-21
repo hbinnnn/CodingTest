1c8ee5c81ae90aad7d972c74013dbdc1f27375b7
hbinnnn <106761437+hbinnnn@users.noreply.github.com>
2024-10-21 23:17:07 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        StringBuilder sb = new StringBuilder(Integer.toString(n, 3));
+        sb.reverse();
+        
+        answer = Integer.parseInt(sb.toString(), 3);
+        
+        return answer;
+    }
+}
