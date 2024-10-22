de7d1d56dc02ab2bb5ec53f6e5f1dd91ec45dd3c
hbinnnn <106761437+hbinnnn@users.noreply.github.com>
2024-10-22 21:35:22 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 66.91 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -7,17 +7,12 @@ class Solution {
         for(int i=0;i<s.length();i++){
             String alpha = s.charAt(i) + "";
             if(!word.contains(alpha)){
-                word += alpha;
                 answer[i] = -1;
+                word += alpha;
             }
             else{
-                for(int j=word.length()-1;j>=0;j--){
-                    if(word.charAt(j) == s.charAt(i)){
-                        word += alpha;
-                        answer[i] = i-j;
-                        break;
-                    }
-                }
+                answer[i] = i - word.lastIndexOf(alpha);
+                word += alpha;
             }
             
         }
