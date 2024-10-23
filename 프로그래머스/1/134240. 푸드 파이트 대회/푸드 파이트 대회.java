b62d54c3b5c39dca051fb559c81adee7d91b2a1a
hbinnnn <106761437+hbinnnn@users.noreply.github.com>
2024-10-23 15:06:36 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 27.23 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i=0;i<food.length;i++){
+            if(food[i] >= 2){
+                for(int j=0;j<food[i]/2;j++){
+                    answer += i;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder(answer);
+        answer += "0"+sb.reverse();
+        
+        return answer;
+    }
+}
